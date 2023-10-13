6255cacd679c0122d8b52797ec4cb63a857d842a
Duc Anh <anhduong764027@gmail.com>
2023-10-13 15:55:27 +0700

    Handle File

---
+++ src/main/java/model/PersonModel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author HP
+ */
+public class PersonModel {
+
+    public PersonModel(String string, String string0, double salary) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/test/Dictionary.java
@@ -0,0 +1,11 @@
+
+package test;
+
+import controller.FileController;
+
+public class Dictionary {
+
+    public static void main(String[] args) {
+        new FileController().run();
+    }
+}
